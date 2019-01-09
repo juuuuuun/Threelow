c8e3f3dc1411766604dc2d5a2463eac79ff21e89
Jun Oh <kyungjunjoh@gmail.com>
2019-01-09 12:33:20 -0800

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Jun Oh on 2019-01-09.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
